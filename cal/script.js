9237ab149df16e68e4701ea583d1346b08342d5e
Marouan-Abdelhaq <127030868+Marouan-Abdelhaq@users.noreply.github.com>
2024-11-22 00:46:25 +0100

    Update script.js

--- cal/script.js
+++ cal/script.js
@@ -1,6 +1,5 @@
 const display = document.getElementById('display');
 
-// Append numbers and operators
 function append(value) {
   if (display.innerText === '0') {
     display.innerText = value;
@@ -9,17 +8,14 @@ function append(value) {
   }
 }
 
-// Clear the display
 function clearDisplay() {
   display.innerText = '0';
 }
 
-// Delete the last character
 function deleteLast() {
   display.innerText = display.innerText.slice(0, -1) || '0';
 }
 
-// Perform calculation
 function calculate() {
   try {
     display.innerText = eval(display.innerText.replace('÷', '/').replace('×', '*'));
